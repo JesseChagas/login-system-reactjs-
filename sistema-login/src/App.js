0581f89455676a8eaef9c99103ae2ebfe8893e2c
Jessé Chagas <Jessechagas2006@gmail.com>
2024-03-11 23:53:56 -0300

    creation of the basic login structure

---
+++ sistema-login/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import GlobalStyle from './styles/global'
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <GlobalStyle/>
+    </>
+  );
+};
+
+export default App
